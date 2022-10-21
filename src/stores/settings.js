73d3f99571094068204e1fa2f9816c64af0a7438
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-10-21 12:08:36 +0200

    update settings state

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia'
 
 export const useSettingsStore = defineStore('settings', {
     state: () => ({
+        identifier   : '',
         congregation : {},
         user         : {},
     }),
     actions: {
         set(settings) {
+            this.identifier   = settings.identifier
             this.congregation = settings.congregation
             this.user         = settings.user
         }
