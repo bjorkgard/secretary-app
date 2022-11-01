e2514f5acf105089c88bc4bab18fac42be980fa1
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-11-01 09:05:31 +0100

    Navigation links take subpages into account

--- src/components/NavigationLink.vue
+++ src/components/NavigationLink.vue
@@ -2,8 +2,8 @@
   <router-link
     :to="`/${to}`"
     :class="[
-      currentRoute === to ? 'border-sky-500 text-slate-900 dark:bg-slate-900 dark:text-white dark:border-transparent' : 'border-transparent text-slate-500 hover:border-slate-300 hover:text-slate-700 dark:text-slate-300 dark:hover:bg-slate-700 dark:hover:text-white dark:hover:border-transparent',
-      'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium dark:px-3 dark:py-2 dark:rounded-md'
+      currentRoute.includes(to) ? 'border-sky-500 text-slate-900 dark:bg-slate-900 dark:text-white dark:border-transparent' : 'border-transparent text-slate-500 hover:border-slate-300 hover:text-slate-700 dark:text-slate-300 dark:hover:bg-slate-700 dark:hover:text-white dark:hover:border-transparent',
+      'inline-flex items-center border-b-2 pl-1 pt-1 text-sm font-medium dark:px-3 dark:py-2 dark:rounded-md'
     ]"
   >
     {{ name }}
