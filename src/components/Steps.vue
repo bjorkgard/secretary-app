c2940163a61952d7341b592f85eb68866a11fbbc
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-11-02 07:44:53 +0100

    Form wizard

---
+++ src/components/Steps.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav
+    aria-label="Progress"
+    class="min-w-full"
+  >
+    <ol
+      role="list"
+      class="flex items-center justify-center"
+    >
+      <li
+        v-for="(step, stepIdx) in steps"
+        :key="step"
+        :class="[stepIdx !== steps - 1 ? 'pr-8 sm:pr-20' : '', 'relative']"
+      >
+        <template v-if="stepIdx < current">
+          <div
+            class="absolute inset-0 flex items-center"
+            aria-hidden="true"
+          >
+            <div class="h-0.5 w-full bg-sky-600" />
+          </div>
+          <span
+            class="relative flex h-8 w-8 items-center justify-center rounded-full bg-sky-600"
+          >
+            <CheckIcon
+              class="h-5 w-5 text-white dark:text-slate-700"
+              aria-hidden="true"
+            />
+          </span>
+        </template>
+        <template
+          v-else-if="stepIdx === current"
+        >
+          <div
+            class="absolute inset-0 flex items-center"
+            aria-hidden="true"
+          >
+            <div class="h-0.5 w-full bg-slate-200 dark:bg-slate-500" />
+          </div>
+          <span
+            class="relative flex h-8 w-8 items-center justify-center rounded-full border-2 border-sky-600 bg-white dark:bg-slate-700"
+            aria-current="step"
+          >
+            <span
+              class="h-2.5 w-2.5 rounded-full bg-sky-600"
+              aria-hidden="true"
+            />
+          </span>
+        </template>
+        <template v-else>
+          <div
+            class="absolute inset-0 flex items-center"
+            aria-hidden="true"
+          >
+            <div class="h-0.5 w-full bg-slate-200 dark:bg-slate-500" />
+          </div>
+          <span
+            class="group relative flex h-8 w-8 items-center justify-center rounded-full border-2 border-slate-300 bg-white hover:border-slate-400 dark:bg-slate-700 dark:border-slate-500"
+          >
+            <span
+              class="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-slate-400"
+              aria-hidden="true"
+            />
+          </span>
+        </template>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script setup>
+import { CheckIcon } from '@heroicons/vue/20/solid'
+
+const props = defineProps({
+    steps   : { type: Number, required: true },
+    current : { type: Number, required: true },
+})
+</script>
---
+++ src/views/PublisherComponent/FormWizard.vue
@@ -0,0 +1,93 @@
+<template>
+  <form @submit="onSubmit">
+    <Steps
+      :steps="stepCounter"
+      :current="currentStepIdx"
+    />
+    <slot />
+    <div class="mt-4 flex justify-end">
+      <SecondaryButton
+        v-if="hasPrevious"
+        label="Föregående"
+        @click="goToPrev"
+      />
+      <Button :label="isLastStep ? 'Spara' : 'Nästa'" />
+    </div>
+
+    <pre>{{ values }}</pre>
+  </form>
+</template>
+
+<script setup>
+import { ref, computed, provide } from 'vue'
+import { useForm }                from 'vee-validate'
+import Button                     from '@/components/form/Button.vue'
+import SecondaryButton            from '@/components/form/SecondaryButton.vue'
+import Steps                      from '@/components/Steps.vue'
+
+const props = defineProps({
+    validationSchema: {
+        type     : Array,
+        required : true,
+    },
+    formValues: {
+        type     : Object,
+        required : true,
+    },
+})
+
+const emit           = defineEmits([ 'submit' ])
+const currentStepIdx = ref(0)
+
+// Injects the starting step, child <form-steps> will use this to generate their ids
+const stepCounter = ref(0)
+provide('STEP_COUNTER', stepCounter)
+
+// Inject the live ref of the current index to child components
+// will be used to toggle each form-step visibility
+provide('CURRENT_STEP_INDEX', currentStepIdx)
+
+// if this is the last step
+const isLastStep = computed(() => {
+    return currentStepIdx.value === stepCounter.value - 1
+})
+
+// If the `previous` button should appear
+const hasPrevious = computed(() => {
+    return currentStepIdx.value > 0
+})
+
+// extracts the indivdual step schema
+const currentSchema = computed(() => {
+    return props.validationSchema[ currentStepIdx.value ]
+})
+
+const { values, handleSubmit } = useForm({
+    initialValues       : props.formValues,
+    // vee-validate will be aware of computed schema changes
+    validationSchema    : currentSchema,
+    // turn this on so each step values won't get removed when you move back or to the next step
+    keepValuesOnUnmount : true,
+})
+
+// We are using the "submit" handler to progress to next steps
+// and to submit the form if its the last step
+const onSubmit = handleSubmit((values) => {
+    if (!isLastStep.value) {
+        currentStepIdx.value++
+
+        return
+    }
+
+    // Let the parent know the form was filled across all steps
+    emit('submit', values)
+})
+
+const goToPrev = () => {
+    if (currentStepIdx.value === 0) {
+        return
+    }
+
+    currentStepIdx.value--
+}
+</script>
