98e2ec7fed5374a65db2db45f4a2aca8a12c9143
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-11-17 23:49:53 +0100

    Add dark mode

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -17,7 +17,7 @@
         leave-from="opacity-100"
         leave-to="opacity-0"
       >
-        <div class="fixed inset-0 bg-slate-500 bg-opacity-75 transition-opacity" />
+        <div class="fixed inset-0 bg-slate-500 bg-opacity-75 transition-opacity dark:bg-opacity-40" />
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
@@ -31,14 +31,14 @@
             leave-from="opacity-100 translate-y-0 sm:scale-100"
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 dark:bg-slate-800">
               <div
                 v-if="closable"
                 class="absolute top-0 right-0 hidden pt-4 pr-4 sm:block"
               >
                 <button
                   type="button"
-                  class="rounded-md bg-white text-slate-400 hover:text-slate-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                  class="rounded-md bg-white text-slate-400 hover:text-slate-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 dark:bg-slate-800"
                   @click="closeDialog"
                 >
                   <XMarkIcon
