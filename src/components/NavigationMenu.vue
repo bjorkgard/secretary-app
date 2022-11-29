8e62119fd951d23ecf8ef4d09dc0a3da7a7bfc0e
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-11-29 22:17:08 +0100

    dark mode

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -4,10 +4,10 @@
       <li class="hover:text-slate-200">
         <router-link
           to="/home"
-          :class="[currentRoute.includes('home') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          :class="[currentRoute.includes('home') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
         >
           <BuildingLibraryIcon
-            :class="[currentRoute.includes('home') ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
+            :class="[currentRoute.includes('home') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
             aria-hidden="true"
           />
           <span class="hidden sm:block ml-3">
@@ -18,10 +18,10 @@
       <li class="hover:text-slate-200">
         <router-link
           to="/publishers"
-          :class="[currentRoute.includes('publishers') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          :class="[currentRoute.includes('publishers') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
         >
           <UsersIcon
-            :class="[currentRoute.includes('publishers') ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
+            :class="[currentRoute.includes('publishers') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
             aria-hidden="true"
           />
           <span class="hidden sm:block ml-3">
@@ -32,10 +32,10 @@
       <li class="hover:text-slate-200">
         <router-link
           to="/exports"
-          :class="[currentRoute.includes('exports') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          :class="[currentRoute.includes('exports') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
         >
           <FolderArrowDownIcon
-            :class="[currentRoute.includes('exports') ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
+            :class="[currentRoute.includes('exports') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
             aria-hidden="true"
           />
           <span class="hidden sm:block ml-3">
