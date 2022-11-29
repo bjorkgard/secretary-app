50accb0a6c9e72cdcd3759ba8d51dde5c08306e4
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-11-29 22:18:58 +0100

    Add tooltip

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -5,6 +5,7 @@
         <router-link
           to="/home"
           :class="[currentRoute.includes('home') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          title="Startsidan"
         >
           <BuildingLibraryIcon
             :class="[currentRoute.includes('home') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
@@ -19,6 +20,7 @@
         <router-link
           to="/publishers"
           :class="[currentRoute.includes('publishers') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          title="Förkunnare"
         >
           <UsersIcon
             :class="[currentRoute.includes('publishers') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
@@ -33,6 +35,7 @@
         <router-link
           to="/exports"
           :class="[currentRoute.includes('exports') ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+          title="Exportera"
         >
           <FolderArrowDownIcon
             :class="[currentRoute.includes('exports') ? 'text-gray-500 dark:text-gray-300' : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300', 'flex-shrink-0 h-6 w-6']"
