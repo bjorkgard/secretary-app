16ce8d85fbb2a8c41ca0d2438b44772498510b3f
Nathanael Björkgård <nathanael@bjorkgard.se>
2023-01-05 13:20:09 +0100

    fix(main): Dark theme on button

--- src/views/PublisherView.vue
+++ src/views/PublisherView.vue
@@ -46,10 +46,10 @@
           </div>
           <button
             type="button"
-            class="relative -ml-px inline-flex items-center rounded-r-md border border-slate-300 bg-slate-50 px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-100 focus:outline-none dark:bg-slate-700 dark:text-slate-400 dark:border-slate-400"
+            class="cursor-pointer relative -ml-px inline-flex items-center rounded-r-md border border-slate-300 bg-slate-50 px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-100 focus:outline-none dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-slate-400 dark:border-slate-400 dark:hover:text-slate-800"
             @click="addPublisher"
           >
-            <span>Lägg till</span>
+            Lägg till
           </button>
         </div>
       </div>
